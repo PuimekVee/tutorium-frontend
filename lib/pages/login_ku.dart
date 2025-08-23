a5a149d7893939df05eb2adabf633703bc1cc3bf
PuimekVee <sirote.v@ku.th>
2025-08-23 23:09:31 +0700

    feat: add KU-ALL login page

---
+++ lib/pages/login_ku.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginKuPage extends StatelessWidget {
+  const LoginKuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Center(
+        child: Text("KU-ALL Login Page")
+      ),
+    );
+  }
+}
